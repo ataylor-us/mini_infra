dbefc45e1ab5c1aaa3ee4df36fdbf32753880fc3
Alex Taylor <ataylor@posteo.us>
2024-11-24 09:14:56 -0500

    moved script to stop containers to repo root

---
+++ stop-containers.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo systemctl disable --now forgejo-container.service
+sudo systemctl disable --now freshrss-container.service
+sudo systemctl disable --now immich-machine-learning-container.service
+sudo systemctl disable --now immich-postgres-container.service
+sudo systemctl disable --now immich-redis-container.service
+sudo systemctl disable --now immich-server-container.service
+sudo systemctl disable --now linkding-container.service
+sudo systemctl disable --now navidrome-container.service
