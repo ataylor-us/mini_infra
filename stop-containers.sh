0532d6512aa7e88d37bcd4bb25af71e96c3d47ba
Alex Taylor <ataylor@posteo.us>
2025-01-08 13:04:08 -0500

    added memos to stop-container script

--- stop-containers.sh
+++ stop-containers.sh
@@ -6,5 +6,6 @@ sudo systemctl disable --now immich-postgres-container.service
 sudo systemctl disable --now immich-redis-container.service
 sudo systemctl disable --now immich-server-container.service
 sudo systemctl disable --now linkding-container.service
+sudo systemctl disable --now memos-container.service
 sudo systemctl disable --now syncthing-container.service
 sudo systemctl disable --now plex-container.service
